53a1d750676e740f60b57113c0c1e1dbf4027027
Ethan <68365423+letsgoawaydev@users.noreply.github.com>
2025-07-05 15:04:08 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/DSIUtil.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, ''),
+  },
+  experiments: {
+        outputModule: true
+  },
+  //mode: "development"
+};
