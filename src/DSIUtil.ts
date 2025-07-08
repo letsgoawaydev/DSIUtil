5e1b3477245bfc0e1359a15c45dc94d74261a5d6
Ethan <68365423+letsgoawaydev@users.noreply.github.com>
2025-07-08 12:51:42 +0800

    fix bugs with dates and if you dont have any photos

--- src/DSIUtil.ts
+++ src/DSIUtil.ts
@@ -15,7 +15,7 @@ export default class DSIUtil {
     currentGridItemSelected: number = -1;
     static griditems: Array<GridItem> = [];
     galleryslider: HTMLInputElement;
-    dsicamerabutton:DSIMenuButton;
+    dsicamerabutton: DSIMenuButton;
     constructor() {
         DSIUtil.instance = this;
         this.dirselect = (document.getElementById("dirselect") as HTMLInputElement);
@@ -24,7 +24,7 @@ export default class DSIUtil {
         this.photodate = (document.getElementById("photodate") as HTMLDivElement);
 
         this.galleryslider = (document.getElementById("galleryslider") as HTMLInputElement);
-        this.dsicamerabutton = new DSIMenuButton(document.getElementById("dsicamerabutton") as HTMLDivElement, ()=>{
+        this.dsicamerabutton = new DSIMenuButton(document.getElementById("dsicamerabutton") as HTMLDivElement, () => {
             (document.getElementById("menuscreen") as HTMLDivElement).style.display = "none";
             (document.getElementById("sdcardscreen-camera") as HTMLDivElement).style.display = "inherit";
             (document.getElementById("sdcardscreen-camera") as HTMLDivElement).style.display = "inherit";
@@ -39,46 +39,55 @@ export default class DSIUtil {
                 this.sdcardbutton.selected = false;
             });
             this.dirselect.addEventListener("change", () => {
-                if (this.dirselect.files != null) {
-                    (document.getElementById("sdcardscreen-camera") as HTMLDivElement).style.display = "none";
-                    (document.getElementById("albumscreen") as HTMLDivElement).style.display = "flex";
-                    document.body.style.backgroundImage = "url(\"looping_album.png\")";
-                    (document.getElementById("navbar") as HTMLDivElement).style.backgroundColor = "#ff5900";
-                    for (let i = 0; i < this.dirselect.files.length; i++) {
-                        const file = this.dirselect.files.item(i);
-                        if (file != null) {
-                            this.parseFile(file);
-                        }
-                    }
-                    this.photos.sort((a, b) => {
-                        return a.photo.lastModified - b.photo.lastModified;
-                    });
-                    (document.getElementById("photoscount") as HTMLDivElement).innerText = "" + this.photos.length;
-                    for (let i = 0; i < this.photos.length; i++) {
-                        let photo = this.photos[i];
-                        if (i != 0) {
-                            let prevPhoto = this.photos[i - 1];
-                            let newPhotoDate = new Date(photo.photo.lastModified);
-                            if (new Date(prevPhoto.photo.lastModified).getDay() != (newPhotoDate.getDay())) {
-                                DSIUtil.griditems.push(new DayGridItem(newPhotoDate));
-                            }
-                        }
-                        else {
-                            DSIUtil.griditems.push(new DayGridItem(new Date(photo.photo.lastModified)));
-                        }
-                        DSIUtil.griditems.push(new PhotoGridItem(photo));
-                    }
-                    DSIUtil.griditems.push(new GridItem(document.getElementById("totalphotos") as HTMLDivElement))
-                    this.setGridItem(DSIUtil.griditems.length - 1);
-                    this.galleryslider.max = "" + (DSIUtil.griditems.length - 1);
-                    this.galleryslider.addEventListener("input", (ev) => {
-                        this.setGridItem(this.galleryslider.valueAsNumber);
-                    });
-                    requestAnimationFrame(DSIUtil.instance.updatePreview);
-                }
+                this.loadPreviewMenu();
             });
         });
     };
+
+    loadPreviewMenu() {
+        if (this.dirselect.files != null) {
+            for (let i = 0; i < this.dirselect.files.length; i++) {
+                const file = this.dirselect.files.item(i);
+                if (file != null) {
+                    this.parseFile(file);
+                }
+            }
+            if (this.photos.length == 0) {
+                this.sdcardbutton.selected = false;
+                alert("No photos were found.")
+                return;
+            }
+            (document.getElementById("sdcardscreen-camera") as HTMLDivElement).style.display = "none";
+            (document.getElementById("albumscreen") as HTMLDivElement).style.display = "flex";
+            document.body.style.backgroundImage = "url(\"looping_album.png\")";
+            (document.getElementById("navbar") as HTMLDivElement).style.backgroundColor = "#ff5900";
+            this.photos.sort((a, b) => {
+                return a.photo.lastModified - b.photo.lastModified;
+            });
+            (document.getElementById("photoscount") as HTMLDivElement).innerText = "" + this.photos.length;
+            for (let i = 0; i < this.photos.length; i++) {
+                let photo = this.photos[i];
+                if (i != 0) {
+                    let prevPhoto = this.photos[i - 1];
+                    let newPhotoDate = new Date(photo.photo.lastModified);
+                    if (new Date(prevPhoto.photo.lastModified).getDay() != (newPhotoDate.getDay())) {
+                        DSIUtil.griditems.push(new DayGridItem(newPhotoDate));
+                    }
+                }
+                else {
+                    DSIUtil.griditems.push(new DayGridItem(new Date(photo.photo.lastModified)));
+                }
+                DSIUtil.griditems.push(new PhotoGridItem(photo));
+            }
+            DSIUtil.griditems.push(new GridItem(document.getElementById("totalphotos") as HTMLDivElement))
+            this.setGridItem(DSIUtil.griditems.length - 1);
+            this.galleryslider.max = "" + (DSIUtil.griditems.length - 1);
+            this.galleryslider.addEventListener("input", (ev) => {
+                this.setGridItem(this.galleryslider.valueAsNumber);
+            });
+            requestAnimationFrame(DSIUtil.instance.updatePreview);
+        }
+    }
 
     setGridItem(id: number) {
         this.currentGridItemSelected = id;
